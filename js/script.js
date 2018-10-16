b8a7e3d8640ff5bfd946022dba0b35901a7632bd
M Michalczyk <pan.michalczyk@gmail.com>
2018-10-16 14:50:47 +0200

    Add tooltip to map

---
+++ js/script.js
@@ -0,0 +1,8 @@
+        $(document).ready(function() {
+            $('.tooltipo').tooltipster({
+//                theme: 'tooltipster-noir',
+                contentAsHTML: true,
+                multiple: false,
+                onlyOne: true
+            });
+        });
