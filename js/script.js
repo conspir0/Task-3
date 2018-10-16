326c8c0f58161811954bf7729859ec784f5248db
M Michalczyk <pan.michalczyk@gmail.com>
2018-10-16 16:05:32 +0200

    Add tooltip to map (follower)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
         $(document).ready(function() {
-            $('.tooltipo').tooltipster({
+            $('.tooltip').tooltipster({
 //                theme: 'tooltipster-noir',
                 contentAsHTML: true,
                 multiple: false,
-                onlyOne: true
+                onlyOne: true,
+                minWidth: 220,
+                minHeight: 1800,
+                offset: [-100, 120],
+                delay: 0,
+                animationDuration: 400,
+                animation: 'fade',
+                trigger: 'hover',
+                plugins: ['follower']
             });
         });
